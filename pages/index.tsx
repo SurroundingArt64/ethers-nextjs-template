996c0704f88d7e3c1e078590ebdbb4c82effb47f
SurroundingArt <97761020+SurroundingArt64@users.noreply.github.com>
2022-04-02 21:32:46 +0530

    feat: setup contract

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { getContract } from '../config/ContractInstance'
+import { useWeb3 } from '../hooks/useWeb3'
 import styles from '../styles/Home.module.css'
+import { ERC20 } from '../types'
 
 const Home: NextPage = () => {
+	const { connectedAddress, incorrectChainId } = useWeb3()
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -10,6 +14,21 @@ const Home: NextPage = () => {
 				<meta name='description' content='' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
+			{connectedAddress && !incorrectChainId && (
+				<>
+					<button
+						onClick={async () => {
+							const ERC20 = await getContract<ERC20>('ERC20')
+							const balance = await ERC20.balanceOf(
+								connectedAddress
+							)
+							console.log({ balance: balance.toString() })
+						}}
+					>
+						Fetch Balance
+					</button>
+				</>
+			)}
 		</div>
 	)
 }
